437f2af33622a885ec233594e282248490fa3073
Palak Mittal <Palak.Mittal@softwire.com>
2022-06-16 15:36:23 +0100

    working on login page

--- myface-ui/src/Components/LoginManager/LoginManager.tsx
+++ myface-ui/src/Components/LoginManager/LoginManager.tsx
@@ -1,37 +1,47 @@
-﻿import React, {createContext, ReactNode, useState} from "react";
+﻿import React, { createContext, ReactNode, useState } from "react";
 
 export const LoginContext = createContext({
-    isLoggedIn: false,
-    isAdmin: false,
-    logIn: () => {},
-    logOut: () => {},
+  isLoggedIn: false,
+  isAdmin: false,
+  logIn: () => {},
+  logOut: () => {},
+  username: "",
+  password: "",
+  setUsername: () => {},
+  setPassword: () => {},
 });
 
 interface LoginManagerProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
 export function LoginManager(props: LoginManagerProps): JSX.Element {
-    const [loggedIn, setLoggedIn] = useState(true);
-    
-    function logIn() {
-        setLoggedIn(true);
-    }
-    
-    function logOut() {
-        setLoggedIn(false);
-    }
-    
-    const context = {
-        isLoggedIn: loggedIn,
-        isAdmin: loggedIn,
-        logIn: logIn,
-        logOut: logOut,
-    };
-    
-    return (
-        <LoginContext.Provider value={context}>
-            {props.children}
-        </LoginContext.Provider>
-    );
+  const [loggedIn, setLoggedIn] = useState(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function logIn() {
+    setLoggedIn(true);
+  }
+
+  function logOut() {
+    setLoggedIn(false);
+  }
+
+  const context = {
+    isLoggedIn: loggedIn,
+    isAdmin: loggedIn,
+    logIn: logIn,
+    logOut: logOut,
+    username: username,
+    password: password,
+    setUsername: () => {},
+    setPassword: () => {},
+  };
+
+  return (
+    <LoginContext.Provider value={context}>
+      {props.children}
+    </LoginContext.Provider>
+  );
 }
